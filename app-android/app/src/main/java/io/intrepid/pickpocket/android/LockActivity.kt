9eae71e20932578e436b5e4aac84d3f99d8ddccf
Russell <russhwolf@gmail.com>
2018-07-01 10:52:29 -0400

    Add empty android project

---
+++ app-android/app/src/main/java/io/intrepid/pickpocket/android/LockActivity.kt
@@ -0,0 +1,12 @@
+package io.intrepid.pickpocket.android
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class LockActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_lock)
+    }
+}
